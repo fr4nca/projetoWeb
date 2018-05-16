f761f84e31a2a50704eb71a1608618e6ee88f40d
Victor França <fr4ncacc@gmail.com>
2018-05-15 21:58:48 -0300

    Alecrim

--- src/app/components/id-itinerario/id-itinerario.component.ts
+++ src/app/components/id-itinerario/id-itinerario.component.ts
@@ -1,103 +1,105 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { RouterModule, Routes, ActivatedRoute } from '@angular/router';
-import { ItinerarioService } from '../../services/itinerario.service'
-import { Itinerario } from '../../models/Itinerario'
-import { Atividade } from '../../models/Atividade'
+import { Component, OnInit, Input } from "@angular/core";
+import { RouterModule, Routes, ActivatedRoute } from "@angular/router";
+import { ItinerarioService } from "../../services/itinerario.service";
+import { Itinerario } from "../../models/Itinerario";
+import { Atividade } from "../../models/Atividade";
 
 @Component({
-  selector: 'app-id-itinerario',
-  templateUrl: './id-itinerario.component.html',
-  styleUrls: ['./id-itinerario.component.css']
+  selector: "app-id-itinerario",
+  templateUrl: "./id-itinerario.component.html",
+  styleUrls: ["./id-itinerario.component.css"]
 })
-
 export class IdItinerarioComponent implements OnInit {
+  id;
+  itinerarios: Itinerario[] = [];
+  itinerario: Itinerario;
+  selectAtividade;
+  rate;
+  vclick = false;
+  porcent = 0;
+  itiLocal;
+  atividade;
 
-  id
-  itinerarios: Itinerario[] = []
-  itinerario: Itinerario
-  selectAtividade
-  rate
-  vclick = false
-  porcent = 0
-  itiLocal
-  atividade
-
-  constructor(private itinerarioService: ItinerarioService, private route: ActivatedRoute) {
-    this.id = this.route.snapshot.params.id
-    this.itinerarioService.getItinerarios().subscribe(iti => this.itinerarios = iti)
+  constructor(
+    private itinerarioService: ItinerarioService,
+    private route: ActivatedRoute
+  ) {
+    this.id = this.route.snapshot.params.id;
+    this.itinerarioService
+      .getItinerarios()
+      .subscribe(iti => (this.itinerarios = iti));
     this.itinerarios.forEach(iti => {
       if (iti.id == this.id) {
-        this.itinerario = iti
+        this.itinerario = iti;
       }
-    })
-    this.rate = this.itinerario.rate_it
+    });
+    this.rate = this.itinerario.rate_it;
   }
 
-  ngOnInit() {
-
-  }
-
+  ngOnInit() {}
 
   setIti(ati) {
-    this.atividade = ati
+    this.atividade = ati;
   }
 
   selecionado(ati) {
     if (this.atividade == ati) {
-      return 'lightgrey'
+      return "lightgrey";
     } else {
-      return 'white'
+      return "white";
     }
   }
 
-
+  onNewAtividade(ativ) {
+    this.selectAtividade = ativ;
+    this.itinerarioService.updateItis(this.itinerarios);
+  }
 
   onSelect(A) {
     this.selectAtividade = A;
   }
 
   rating_porcent() {
-    this.porcent = this.itinerario.likect / this.itinerario.avaliacao
+    this.porcent = this.itinerario.likect / this.itinerario.avaliacao;
     if (this.porcent <= 0.2) {
-      this.rate = 1
+      this.rate = 1;
     } else if (this.porcent > 0.2 && this.porcent <= 0.4) {
-      this.rate = 2
+      this.rate = 2;
     } else if (this.porcent > 0.4 && this.porcent <= 0.6) {
-      this.rate = 3
+      this.rate = 3;
     } else if (this.porcent > 0.6 && this.porcent <= 0.8) {
-      this.rate = 4
+      this.rate = 4;
     } else if (this.porcent > 0.8) {
-      this.rate = 5
+      this.rate = 5;
     }
 
-    this.itinerario.rate_it = this.rate
-    this.itinerarioService.updateItis(this.itinerarios)
+    this.itinerario.rate_it = this.rate;
+    this.itinerarioService.updateItis(this.itinerarios);
   }
 
   like() {
-    this.vclick = true
-    this.itinerario.likect += 1
-    this.itinerario.avaliacao += 1
-    this.itinerarioService.updateItis(this.itinerarios)
-    this.rating_porcent()
-    console.log("Entrei no like " + this.itinerario.likect)
-    console.log("CtAvaliação " + this.itinerario.avaliacao)
-    console.log("Porcentagem " + this.porcent)
+    this.vclick = true;
+    this.itinerario.likect += 1;
+    this.itinerario.avaliacao += 1;
+    this.itinerarioService.updateItis(this.itinerarios);
+    this.rating_porcent();
+    console.log("Entrei no like " + this.itinerario.likect);
+    console.log("CtAvaliação " + this.itinerario.avaliacao);
+    console.log("Porcentagem " + this.porcent);
   }
 
   dislike() {
-    this.vclick = true
-    this.itinerario.avaliacao += 1
-    this.itinerarioService.updateItis(this.itinerarios)
-    this.rating_porcent()
-    console.log("Entrei no dislike " + this.itinerario.likect)
-    console.log("CtAvaliação " + this.itinerario.avaliacao)
-    console.log("Porcentagem " + this.porcent)
+    this.vclick = true;
+    this.itinerario.avaliacao += 1;
+    this.itinerarioService.updateItis(this.itinerarios);
+    this.rating_porcent();
+    console.log("Entrei no dislike " + this.itinerario.likect);
+    console.log("CtAvaliação " + this.itinerario.avaliacao);
+    console.log("Porcentagem " + this.porcent);
   }
 
   done(ativ) {
-    ativ.done = !ativ.done
-    this.itinerarioService.updateItis(this.itinerarios)
+    ativ.done = !ativ.done;
+    this.itinerarioService.updateItis(this.itinerarios);
   }
-
 }
--- src/app/components/inserir-dicas/inserir-dicas.component.ts
+++ src/app/components/inserir-dicas/inserir-dicas.component.ts
@@ -1,45 +1,45 @@
-import { Component, OnInit, Input } from '@angular/core'
-import { Itinerario } from '../../models/Itinerario'
-import { User } from '../../models/User'
-import { Atividade } from '../../models/Atividade'
-import { Dica } from '../../models/Dica'
-import { ItinerarioService } from '../../services/itinerario.service'
+import { Component, OnInit, Input, Output, EventEmitter } from "@angular/core";
+import { Itinerario } from "../../models/Itinerario";
+import { User } from "../../models/User";
+import { Atividade } from "../../models/Atividade";
+import { Dica } from "../../models/Dica";
+import { ItinerarioService } from "../../services/itinerario.service";
 
 @Component({
-  selector: 'app-inserir-dicas',
-  templateUrl: './inserir-dicas.component.html',
-  styleUrls: ['./inserir-dicas.component.css']
+  selector: "app-inserir-dicas",
+  templateUrl: "./inserir-dicas.component.html",
+  styleUrls: ["./inserir-dicas.component.css"]
 })
-
 export class InserirDicasComponent implements OnInit {
+  dicas: Dica[] = [];
+  dica: Dica;
+  dDica: string;
+  dUsuario: string;
+  usuario: User;
+  @Input() selectAtividade: Atividade;
+  @Output() newAtividade = new EventEmitter();
 
-  dicas: Dica[] = []
-  dica: Dica
-  dDica: string
-  dUsuario: string
-  usuario: User
-  @Input() selectAtividade: Atividade
+  constructor(private itinerarioService: ItinerarioService) {}
 
-  constructor(private itinerarioService: ItinerarioService) { }
+  ngOnInit() {}
 
-  ngOnInit() {
-  }
-
-  addDica(dDica){
-    if(this.selectAtividade != undefined){
-      if(this.dDica != undefined){
-        console.log(this.dDica)
-        console.log(this.selectAtividade)
-        this.dica = { conteudo: this.dDica, usuario: { nome: this.dUsuario, email: ''}}
-        this.selectAtividade.dicas.push(this.dica)
-        this.dDica = ''
-      }else{
-        alert("Escreva uma dica.")
+  addDica() {
+    if (this.selectAtividade != undefined) {
+      if (this.dDica != undefined) {
+        console.log(this.dDica);
+        console.log(this.selectAtividade);
+        this.dica = {
+          conteudo: this.dDica,
+          usuario: { nome: this.dUsuario, email: "" }
+        };
+        this.selectAtividade.dicas.push(this.dica);
+        this.dDica = "";
+        this.newAtividade.emit(this.selectAtividade);
+      } else {
+        alert("Escreva uma dica.");
       }
-    }else{
-      alert("Selecione uma atividade!")
+    } else {
+      alert("Selecione uma atividade!");
     }
   }
-
-
 }
