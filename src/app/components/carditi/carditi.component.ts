34b11b24e09adbfa2c15f1eb379d22102ab31477
Victor França <fr4ncacc@gmail.com>
2018-05-16 10:05:59 -0300

    Refatorando

--- src/app/components/carditi/carditi.component.ts
+++ src/app/components/carditi/carditi.component.ts
@@ -1,75 +1,57 @@
-import { Component, OnInit, Input } from '@angular/core'
-import { RouterModule, Routes, ActivatedRoute } from '@angular/router'
-import { ItinerarioService } from '../../services/itinerario.service'
-import { Itinerario } from '../../models/Itinerario'
-import { Atividade } from '../../models/Atividade'
-import { MOCK } from '../mock'
+import { Component, OnInit, Input } from "@angular/core";
+import { RouterModule, Routes, ActivatedRoute } from "@angular/router";
+import { ItinerarioService } from "../../services/itinerario.service";
+import { Itinerario } from "../../models/Itinerario";
+import { Atividade } from "../../models/Atividade";
 
 @Component({
-  selector: 'app-carditi',
-  templateUrl: './carditi.component.html',
-  styleUrls: ['./carditi.component.css']
+  selector: "app-carditi",
+  templateUrl: "./carditi.component.html",
+  styleUrls: ["./carditi.component.css"]
 })
-
 export class CarditiComponent implements OnInit {
-  //@Input() itinerario
+  @Input() itinerario;
+  itinerarios: Itinerario[] = [];
 
-  @Input() itinerario.rate_it;
-  //rate = this.itinerario.rate_it
-  itinerarios : Itinerario[] = []
-  itinerario : Itinerario
-  itinerariosRank5 : ItinerariosRank5[] = []
-  itinerariosRank4 : ItinerariosRank4[] = []
-  //rank5 : rank5[] = []
-  //rank4 : rank4[] = []
-  //rank3 : rank3[] = []
-  //rank2 : rank2[] = []
-  //rank1 : rank1[] = []
-  rate
-  id
+  rate;
+  id;
 
-    constructor(private itinerarioService: ItinerarioService) {
-    }
+  constructor(private itinerarioService: ItinerarioService) {}
 
-  ranking(){
-    let i : number
-    let z : number = 0;
-    let w : number = 0;
-    for(i = 0; i <= this.itinerarios.length; i++){
-      if(this.itinerario.rate_it == 5 && this.itinerario.id == i + 1){
-         this.itinerariosRank5[z] = this.itinerarios[i];
-         z = z + 1;
-      }else{
-        if(this.itinerario.rate_it == 4 && this.itinerario.id == i + 1){
-            this.itinerariosRank4[i] = this.itinerarios[i];
-            //w = w + 1;
-        }
-      }
-    }
-    console.log("I--------------------");
-    console.log("Rank 5: "  this.itinerariosRank5);
-    console.log("Rank 4: "  this.itinerariosRank4);
-    console.log("z:" z);
-    console.log("w:" w);
-    console.log("F--------------------");
+  // ranking(){
+  //   let i : number
+  //   let z : number = 0;
+  //   let w : number = 0;
+  //   for(i = 0; i <= this.itinerarios.length; i++){
+  //     if(this.itinerario.rate_it == 5 && this.itinerario.id == i + 1){
+  //        this.itinerariosRank5[z] = this.itinerarios[i];
+  //        z = z + 1;
+  //     }else{
+  //       if(this.itinerario.rate_it == 4 && this.itinerario.id == i + 1){
+  //           this.itinerariosRank4[i] = this.itinerarios[i];
+  //           //w = w + 1;
+  //       }
+  //     }
+  //   }
+  //   console.log("I--------------------");
+  //   console.log("Rank 5: "  this.itinerariosRank5);
+  //   console.log("Rank 4: "  this.itinerariosRank4);
+  //   console.log("z:" z);
+  //   console.log("w:" w);
+  //   console.log("F--------------------");
 
+  // }
+
+  getItinerarios(): void {
+    this.itinerarioService
+      .getItinerarios()
+      .subscribe(itinerarios => (this.itinerarios = itinerarios));
+    this.rate = this.itinerario.rate_it;
   }
-
-  getItinerarios(): void{
-  	 this.itinerarioService.getItinerarios()
-  		.subscribe(itinerarios => this.itinerarios = itinerarios);
-  		this.rate = this.itinerario.rate_it
-  }
-
 
   ngOnInit() {
     this.getItinerarios();
-  	console.log(this.itinerario);
+    console.log(this.itinerario);
     console.log(this.itinerario.rate_it);
-    //this.ranking();
-
   }
-
-
-
 }
--- src/app/components/index/index.component.ts
+++ src/app/components/index/index.component.ts
@@ -1,28 +1,22 @@
-import { Component, OnInit } from '@angular/core';
-import { MOCK } from '../mock'
-import { ItinerarioService } from '../../services/itinerario.service'
-import { RouterModule, Routes, ActivatedRoute } from '@angular/router';
-import { Itinerario } from '../../models/Itinerario'
-import { Atividade } from '../../models/Atividade'
+import { Component, OnInit } from "@angular/core";
+import { ItinerarioService } from "../../services/itinerario.service";
+import { RouterModule, Routes, ActivatedRoute } from "@angular/router";
+import { Itinerario } from "../../models/Itinerario";
+import { Atividade } from "../../models/Atividade";
 
 @Component({
-  selector: 'app-index',
-  templateUrl: './index.component.html',
-  styleUrls: ['./index.component.css']
+  selector: "app-index",
+  templateUrl: "./index.component.html",
+  styleUrls: ["./index.component.css"]
 })
-
 export class IndexComponent implements OnInit {
-
-  itinerarios
+  itinerarios;
 
   constructor(private itinerarioService: ItinerarioService) {
     this.itinerarioService.getItinerarios().subscribe(iti => {
-      this.itinerarios = iti
-    })
+      this.itinerarios = iti;
+    });
   }
 
-  ngOnInit() {
-
-  }
-
+  ngOnInit() {}
 }
