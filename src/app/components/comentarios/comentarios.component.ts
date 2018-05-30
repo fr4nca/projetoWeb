3c0023fbfa4b50c21101c9de63fa85315bf9c286
thaygneel <thaycic@hotmail.com>
2018-05-30 01:36:59 -0300

    Comentarios ~Thay

--- src/app/components/comentarios/comentarios.component.ts
+++ src/app/components/comentarios/comentarios.component.ts
@@ -2,8 +2,9 @@ import { Component, OnInit, Input } from '@angular/core';
 import { Comentario } from "../../models/Comentario";
 import { User } from "../../models/User";
 import { Itinerario } from "../../models/Itinerario";
-
-
+import { ItinerarioService } from "../../services/itinerario.service";
+import { RouterModule, Routes, ActivatedRoute } from "@angular/router";
+import { HttpClient } from "@angular/common/http";
 
 @Component({
   selector: 'app-comentarios',
@@ -11,16 +12,31 @@ import { Itinerario } from "../../models/Itinerario";
   styleUrls: ['./comentarios.component.css']
 })
 export class ComentariosComponent implements OnInit {
+  id
   @Input() vcoment
   nUsuario: string
   descricao: string
   coment: Comentario
   usuario: User;
   vAlert = false;
+  itinerario: Itinerario;
 
-  constructor() { }
+  constructor(
+    private itinerarioService: ItinerarioService,
+    private route: ActivatedRoute,
+    private http: HttpClient
+  ) { }
 
   ngOnInit() {
+    this.id = this.route.snapshot.params.id;
+    this.getIti();
+  }
+
+  getIti() {
+    this.itinerarioService.getItinerario(this.id).subscribe(iti => {
+      this.itinerario = iti;
+      this.vcoment = this.itinerario.comentarios;
+    });
   }
 
   addComentario(){
@@ -30,6 +46,7 @@ export class ComentariosComponent implements OnInit {
           usuario: {nome: this.nUsuario, email: ""}
         };
         this.vcoment.push(this.coment);
+        this.itinerarioService.updateIti(this.itinerario).subscribe(data => data);
   }
 
   getAlert(){
