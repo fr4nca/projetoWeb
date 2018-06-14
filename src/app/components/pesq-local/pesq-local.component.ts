b41c2c459ac5752492cb190fc68720119fcd7377
Victor França <fr4ncacc@gmail.com>
2018-06-14 00:01:37 -0300

    final

--- src/app/components/pesq-local/pesq-local.component.ts
+++ src/app/components/pesq-local/pesq-local.component.ts
@@ -1,52 +1,49 @@
-import { Component, OnInit, Input} from '@angular/core';
+import { Component, OnInit, Input } from "@angular/core";
 import { RouterModule, Routes, ActivatedRoute } from "@angular/router";
 import { Itinerario } from "../../models/Itinerario";
 import { Local } from "../../models/Local";
 import { ItinerarioService } from "../../services/itinerario.service";
-import {} from "googlemaps";
 import { MapsAPILoader } from "@agm/core";
 
 @Component({
-  selector: 'app-pesq-local',
-  templateUrl: './pesq-local.component.html',
-  styleUrls: ['./pesq-local.component.css']
+  selector: "app-pesq-local",
+  templateUrl: "./pesq-local.component.html",
+  styleUrls: ["./pesq-local.component.css"]
 })
 export class PesqLocalComponent implements OnInit {
-
   pesqIti: Itinerario[];
   pItinerario: Itinerario[];
   pesqCity;
-  pLocal
-  pesqLocal
-  arrayLocais: string[]=[];
+  pLocal;
+  mensagem;
+  t;
+  arrayLocais: string[] = [];
 
-  constructor(private itiService: ItinerarioService, private mapsAPILoader: MapsAPILoader) {
+  constructor(
+    private itiService: ItinerarioService,
+    private mapsAPILoader: MapsAPILoader
+  ) {}
 
-  }
+  ngOnInit() {}
 
-  ngOnInit() {
-  }
+  pesquisarCidade() {
+    this.pItinerario = [];
+    this.pLocal = false;
 
-  pesquisarCidade(city){
-    this.pesqCity = city;
-    this.pItinerario=[];
-    this.arrayLocais=[];
-    this.pLocal=false;
+    this.itiService.getItinerarios().subscribe(itinera => {
+      this.pesqIti = itinera;
 
-    this.itiService.getItinerarios().subscribe(itinera => { this.pesqIti = itinera;
-
-      for(var i=0; i<this.pesqIti.length; i++){
-        this.arrayLocais.push(this.pesqIti[i].local.local);
-        if(this.pesqIti[i].local.local.includes(this.pesqCity)){
+      for (var i = 0; i < this.pesqIti.length; i++) {
+        if (this.pesqIti[i].local.local.includes(this.pesqCity)) {
           this.pItinerario.push(this.pesqIti[i]);
-          this.pLocal = true
+          this.pLocal = true;
         }
       }
+      if (this.pLocal) {
+        this.mensagem = false;
+      } else {
+        this.mensagem = true;
+      }
     });
-    if(this.pLocal){
-      this.pesqLocal=true;
-    }else{
-      this.pesqLocal= false;
-    }
   }
 }
